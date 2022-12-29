1e4bb05df1fcdbb67b32b1da2b83e4d5deae7fcc
David Lee <david@Davids-MacBook-Pro.local>
2022-12-29 13:43:00 +0800

    feat: save and delete cards to CoreData

--- SpendingTracker/Main/MainView.swift
+++ SpendingTracker/Main/MainView.swift
@@ -9,27 +9,38 @@ import SwiftUI
 
 struct MainView: View {
   @State private var shouldPresentAddCardForm = false
+  @Environment(\.managedObjectContext) private var viewContext
+  @FetchRequest(
+    sortDescriptors: [NSSortDescriptor(
+      keyPath: \Card.timestamp,
+      ascending: true)],
+    animation: .default
+  )
+  private var cards: FetchedResults<Card>
   
   var body: some View {
     NavigationView {
       ScrollView {
-        TabView {
-          ForEach(0 ..< 5) { num in
-            CreditCardView()
-              .padding(.bottom, 50)
+        if !cards.isEmpty {
+          TabView {
+            ForEach(cards) { card in
+              CreditCardView()
+                .padding(.bottom, 50)
+            }
           }
+          .tabViewStyle(
+            PageTabViewStyle(
+              indexDisplayMode: .always
+            )
+          )
+          .frame(height: 280)
+          .indexViewStyle(
+            PageIndexViewStyle(
+              backgroundDisplayMode: .always
+            )
+          )
         }
-        .tabViewStyle(
-          PageTabViewStyle(
-            indexDisplayMode: .always
-          )
-        )
-        .frame(height: 280)
-        .indexViewStyle(
-          PageIndexViewStyle(
-            backgroundDisplayMode: .always
-          )
-        )
+        
         Spacer()
           .fullScreenCover(
             isPresented: $shouldPresentAddCardForm, onDismiss: nil) {
@@ -37,7 +48,46 @@ struct MainView: View {
             }
       }
       .navigationTitle("Credit Cards")
-      .navigationBarItems(trailing:  addCardButton)
+      .navigationBarItems(
+        leading: HStack {
+          addItemButton
+          deleteAllButton
+        },
+        trailing: addCardButton
+      )
+    }
+  }
+  
+  var addItemButton: some View {
+    Button {
+      withAnimation {
+        let viewContext = PersistenceController.shared.container.viewContext
+        let card = Card(context: viewContext)
+        card.timestamp = Date()
+        
+        do {
+          try viewContext.save()
+        } catch {
+          
+        }
+      }
+    } label: {
+      Text("Add Item")
+    }
+  }
+  
+  private var deleteAllButton: some View {
+    Button {
+      cards.forEach { card in
+        viewContext.delete(card)
+      }
+      do {
+        try viewContext.save()
+      } catch {
+        
+      }
+    } label: {
+      Text("Delete All")
     }
   }
   
@@ -108,6 +158,8 @@ struct CreditCardView: View {
 
 struct MainView_Previews: PreviewProvider {
   static var previews: some View {
+    let viewContext = PersistenceController.shared.container.viewContext
     MainView()
+      .environment(\.managedObjectContext, viewContext)
   }
 }
