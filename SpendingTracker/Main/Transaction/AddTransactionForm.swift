ed3a1616ba401619863e5f3bc1cc64b4d6be0a4c
David Lee <david@Davids-MacBook-Pro.local>
2022-12-30 18:56:07 +0800

    feat: add many to many relationships

--- SpendingTracker/Main/Transaction/AddTransactionForm.swift
+++ SpendingTracker/Main/Transaction/AddTransactionForm.swift
@@ -86,6 +86,7 @@ struct AddTransactionForm: View {
       transaction.photoData = self.photoData
       
       transaction.card = self.card
+      transaction.categories = self.selectedCategories as NSSet
       
       do {
         try context.save()
